2e79ac937c30a61dc91a01b62e1d25c23b505dbc
snaulX <alexandergunger666@gmail.com>
2022-03-22 22:06:06 +0600

    Made the design better

--- app/src/main/java/com/snaulx/roadmap/MainActivity.kt
+++ app/src/main/java/com/snaulx/roadmap/MainActivity.kt
@@ -3,11 +3,12 @@ package com.snaulx.roadmap
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.ColorInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val rectStyle = RectStyle(Color.CYAN, 30F, 300F, 100F, 10F, 20F)
+        title = "Android Roadmap"
         val roadmap = tree<String> {
             node("Pick a language") {
                 branch("Kotlin")
@@ -15,29 +16,27 @@ class MainActivity : AppCompatActivity() {
             }
             node("Fundamentals") {
                 branch("Install Android Studio") {
-                    branch("Visit site") {
-                        branch("A")
-                    }
-                    branch("B") {
-                        branch("C")
-                    }
+                    branch("Visit site")
+                    branch("Download")
                 }
                 branch("Learn Language") {
-                    branch("Learn Basics of Language", "Learn OOP")
+                    branch("Learn Basics", "Learn OOP")
                 }
                 branch("Left")
                 branch("Right")
             }
             node("Version Control Systems", "Repo Hosting Services")
         }.paintTree(padding = 30F,
-            lineColor = Color.rgb(255, 159, 43), lineWidth = 4F,
+            lineColor = Color.rgb(129, 43, 178), lineWidth = 4F,
             textColor = Color.BLACK,
-            NodeStyle(10F, rectStyle, 20F),
-            BranchStyle(10F, rectStyle, 30F),
-            BranchStyle(5F, rectStyle, 10F),
-            BranchStyle(2F, rectStyle, 15F),
+            NodeStyle(6.7F, makeRectStyle(Color.rgb(255, 228, 151), 400F), 40F),
+            BranchStyle(7.5F, makeRectStyle(Color.rgb(255, 255, 7)), 30F),
+            BranchStyle(5F, makeRectStyle(Color.rgb(204, 204, 204)), 20F),
+            BranchStyle(5F, makeRectStyle(Color.rgb(255, 194, 36)), 25F),
         )
         //setContentView(R.layout.activity_main)
         setContentView(RoadmapView(this, roadmap))
     }
+
+    private fun makeRectStyle(@ColorInt color: Int, width: Float = 300F) = RectStyle(color, 31F, width, 100F, 10F, 20F)
 }
